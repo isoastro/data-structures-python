d011456b78b6c7f3a2b077adcd76f72ec6ca1b53
Andrew Curtis <a.curtis.91@gmail.com>
2018-11-19 21:27:05 -0800

    Extra spaces for PEP-ness

--- TrieIterative.py
+++ TrieIterative.py
@@ -5,13 +5,16 @@ class TrieNode:
         self._children = []
         self._end = 0
 
+
     def add_child(self, child: 'TrieNode'):
         '''Adds a child TrieNode to the children of this TrieNode'''
         self._children.append(child)
 
+
     def mark_end(self):
         '''Identifies that this node marks the end of a word or list'''
         self._end += 1
+
 
     def __str__(self):
         s = f'{str(self._value)}'
@@ -19,10 +22,12 @@ class TrieNode:
             s += '*'
         return s
 
+
 class Trie:
     def __init__(self):
         '''Creates a new trie containing a single root node'''
         self._root = TrieNode('ROOT')
+
 
     def trace(self, iterable):
         '''Generates a list of indices for each layer where each element of the given iterable is found'''
@@ -35,6 +40,7 @@ class Trie:
                     break
             else:
                 raise ValueError('Given iterable not found in Trie')
+
 
     def add(self, iterable):
         '''Adds an iterable to the trie, iterating down'''
@@ -50,12 +56,14 @@ class Trie:
                 node = new_node
         node.mark_end()
 
+
     # TODO: Implement
     # General approach is to get the location of each node in the Trie, and go
     # to the bottom. Once you're at the bottom, delete a node if it doesn't
     # have any children, and move up a layer. Repeat
     def remove(self, iterable):
         raise NotImplementedError
+
 
     def find_prefix(self, iterable):
         '''Finds if a given iterable is found within the trie'''
@@ -72,6 +80,7 @@ class Trie:
                 return None
         return node
 
+
     def has_iterable(self, iterable):
         '''Returns True if the iterable (as an entire sequence) is found within the trie, otherwise False'''
         node = self.find_prefix(iterable)
@@ -79,6 +88,7 @@ class Trie:
             return bool(node._end)
         return False
 
+
     def __contains__(self, iterable):
         '''Returns True if the iterable is found within the trie, otherwise False'''
-        return self.find_prefix(iterable) is not None    
+        return self.find_prefix(iterable) is not None
